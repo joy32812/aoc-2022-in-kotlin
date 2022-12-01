ea216722fea0a1266ca4ba2840a1061c284b61b9
xiaoyuan <joy32812@qq.com>
2022-12-01 20:25:42 +0100

    AoC Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
+
+    fun List<String>.toCalories(): List<Int> {
+        return joinToString(",")
+            .split(",,")
+            .map { it.split(",").sumOf { it.toInt() } }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.toCalories().max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.toCalories().sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    println(part2(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
