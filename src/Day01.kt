5eea74ea4a60ef0e4c35ee5dde2d2ed38d93cd30
xiaoyuan <joy32812@qq.com>
2022-12-03 10:47:13 +0100

    add test data for day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,11 +15,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("data/Day01_test")
     check(part1(testInput) == 24000)
     println(part2(testInput))
 
-    val input = readInput("Day01")
+    val input = readInput("data/Day01")
     println(part1(input))
     println(part2(input))
 }
